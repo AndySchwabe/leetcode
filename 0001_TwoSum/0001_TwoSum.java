e0d8bff405966d795d223ee97187f158dd0b3e69
Andy Schwabe <AndySchwabe@users.noreply.github.com>
2019-04-15 15:31:49 -0400

    0001 - Two Sum (#1)
    * Working two sum code added
    
    * Fixed naming to adjust for 1000+ tests

--- 0001_TwoSum/0001_TwoSum.java
+++ 0001_TwoSum/0001_TwoSum.java
@@ -1,4 +1,9 @@
 class Solution {
+/**
+ * https://leetcode.com/problems/two-sum/
+ * Runtime: 74 ms, faster than 5.03% of Java online submissions for Two Sum.
+ * Memory Usage: 38.4 MB, less than 52.65% of Java online submissions for Two Sum.
+ */
     public int[] twoSum(int[] nums, int target) {
     
         int solution[] = new int[2];
@@ -9,7 +14,7 @@ class Solution {
             result = (target - nums[i]);
             
             for (int j = 0; j < nums.length; j++){
-                if (result == nums[j]){
+                if ((result == nums[j]) && (i != j)){
                     solution[0] = i;
                     solution[1] = j;
                     break outerloop;
