9e32e2674d50ba18d662c88c32f876a6dfe4bbeb
Andy Schwabe <andy.schwabe@infor.com>
2019-04-18 16:53:35 -0400

    Pushing PalindromeNumber

--- src/main/java/com/ts/problems/easy/palindromenumber/PalindromeNumber.java
+++ src/main/java/com/ts/problems/easy/palindromenumber/PalindromeNumber.java
@@ -1,12 +1,36 @@
 /**
  * https://leetcode.com/problems/palindrome-number/
+ * Runtime: 10 ms, faster than 80.59% of Java online submissions for Palindrome Number.
+ * Memory Usage: 35.8 MB, less than 100.00% of Java online submissions for Palindrome Number.
  */
 
 package com.ts.problems.easy.palindromenumber;
 
 class PalindromeNumber {
 	public boolean isPalindrome(int x) {
+		boolean result;
+		String inputString; // string to store casted int in
+		String[] inputArray; // array to store chars in
+		result = false; //guilty until proven innocent
 		
-		return true;
+		inputString = Integer.toString(x);
+		
+		inputArray = new String[inputString.length()];
+		
+		for (int i = 0; i < inputString.length(); i++) {
+			inputArray[i] = inputString.substring(i, (i+1));
+		}
+		
+		for (int j = 0; j < inputArray.length; j++) {
+			if (inputArray[j].equals(inputArray[((inputArray.length - j) - 1)])) {
+				result = true;
+			}
+			else{
+				result = false;
+				break;
+			}
+		}
+		
+		return result;
 	}
 }
