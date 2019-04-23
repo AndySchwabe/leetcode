c64751245d9ab5b7affa496c4063221f81a8a352
Andy Schwabe <andy.schwabe@infor.com>
2019-04-23 11:09:50 -0400

    update to release.py

--- release.py
+++ release.py
@@ -13,6 +13,11 @@ class Release:
 
         self.version = args.version
 
+        if self.version.startswith('v'):
+            pass
+        else:
+            self.version = 'v' + self.version
+
         self.repo = gitapi.Repo("/Users/andy/leetcode")
 
         status = self.repo.git_status()
@@ -38,10 +43,10 @@ class Release:
             if line.startswith("  file: 'target/leetcode"):
                 if line[25 : (len(line) - 6)] > self.version:
                     sys.exit(
-                        "Your travis version number is higher than the desired version number. Exiting..."
+                        "Your current travis version number is higher than the desired version number. Exiting..."
                     )
                 else:
-                    line = line.replace(line[25 : (len(line) - 6)], str(self.version))
+                    line = line.replace(line[25 : (len(line) - 6)], self.version)
             new_travis += line
 
         new_travis = "".join(new_travis)
@@ -60,10 +65,10 @@ class Release:
             if line.startswith("\t<version>"):
                 if line[10 : (len(line) - 11)] > self.version:
                     sys.exit(
-                        "Your pom version number is higher than the desired version number. Exiting..."
+                        "Your current pom version number is higher than the desired version number. Exiting..."
                     )
                 else:
-                    line = line.replace(line[10 : (len(line) - 11)], str(self.version))
+                    line = line.replace(line[10 : (len(line) - 11)], self.version)
             new_pom += line
 
         new_pom = "".join(new_pom)
@@ -73,13 +78,18 @@ class Release:
 
     def commit_and_tag(self):
         """Method to stage changes, commit changes, tag commit, and push to origin"""
-
+        self.repo.git_add("pom.xml")
+        self.repo.git_add(".travis.yml")
+        commit_message = "Updating version to: " + self.version
+        self.repo.git_commit(commit_message, user="Andy Schwabe <andy.schwabe@infor.com>", ".")
+        self.repo.git_tag(self.version)
+        self.repo.git_push()
 
 
 def main():
     """Where the magic happens"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("version", type=float)
+    parser.add_argument("version", type=str)
     args = parser.parse_args()
 
     releaser = Release(args)
